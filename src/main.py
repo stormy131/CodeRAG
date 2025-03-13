7b3388f760f9d16294f51f1bdc447f8dafe9a892
stormy131 <artemkopyl1990@gmail.com>
2025-03-13 07:44:57 +0100

    baseline rag

--- src/main.py
+++ src/main.py
@@ -1,14 +1,32 @@
+from asyncio import get_event_loop
 from pathlib import Path
 
 from git import Repo
+from dotenv import load_dotenv
+
+from rag.rag import RAGExtractor
+from utils.data import load_docs, get_chunks
 
 
-GIT_REPO = "https://github.com/viarotel-org/escrcpy.git"
-
+load_dotenv()
 # TODO: Path config
-def main():
-    Repo.clone_from(GIT_REPO, Path("../data"))
+GIT_REPO = "https://github.com/viarotel-org/escrcpy.git"
+DATA_ROOT = Path("./data/fetched")
+
+
+async def main():
+    if not DATA_ROOT.exists():
+        Repo.clone_from(GIT_REPO, DATA_ROOT)
+
+    documents = load_docs(DATA_ROOT)
+    chunked = await get_chunks(documents)
+    rag = RAGExtractor(chunked)
+
+    query = "How is the wireless pairing dialog functionality implemented?"
+    result = await rag.ainvoke(query)
+    breakpoint()
 
 
 if __name__ == "__main__":
-    main()
+    loop = get_event_loop() 
+    loop.run_until_complete( main() )
---
+++ src/rag/rag.py
@@ -0,0 +1,43 @@
+from typing import TypedDict
+
+from langchain_core.documents import Document
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langgraph.graph import StateGraph, START
+
+
+class RAGState(TypedDict):
+    question: str
+    retrieval: list[str]
+
+
+class RAGExtractor:
+    """
+    TODO: add docstring
+    """
+
+    def __init__(self, docs_pool: list[Document]):
+        embeddings = GoogleGenerativeAIEmbeddings( model="models/text-embedding-004" )
+        self._vector_store = InMemoryVectorStore(embeddings)
+        self._vector_store.add_documents(docs_pool)
+
+        builder = StateGraph(RAGState).add_node("retrieve", self._retrieve)
+        builder.add_edge(START, "retrieve")
+        self._graph = builder.compile()
+
+
+    # TODO: search result pre-filtering
+    async def _retrieve(self, state: RAGState) -> RAGState:
+        relevant_docs = await self._vector_store.asimilarity_search(state["question"], k=5)
+        return state | {
+            "retrieval": [d.metadata["source"] for d in relevant_docs]
+        }
+
+
+    async def ainvoke(self, query: str) -> list[str]:
+        search_result = await self._graph.ainvoke({"question": query})
+        return search_result["retrieval"]
+
+
+if __name__ == "__main__":
+    pass
