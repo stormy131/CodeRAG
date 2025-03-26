f8283e5c2cf151eda8e03bf90955995506cba7a2
stormy131 <artemkopyl1990@gmail.com>
2025-03-26 08:43:55 +0100

    bm25 for hybrid search + pretrain

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 Things to try out:
 - Hybrid search
     - Combine __semantic__ and __keywrod based__ search together
-    - could be on words _function, authentication, user, ..._
     - BM25 + builin search
 - Query expansion
     - integrate LLM calling for generating a __refined prompt__
     - "where is the auth habdled" -> "find functions related to user's ..."
 - Advanced embedding
     - Use BERT based encoders to create file embeddings
+    - Requires aliignment / fine-tune ??
 - Indexing algorithms
     - Hierarchical Navigable Small World (HNSW) index ?
 - Graph based retrieval with GNNs ??
--- src/rag/_rag.py
+++ src/rag/_rag.py
@@ -1,14 +1,19 @@
-import pickle
 from typing import TypedDict
 
 import faiss
+from langchain.retrievers import EnsembleRetriever, BM25Retriever
 from langchain.vectorstores import FAISS
 from langchain_core.documents import Document
 from langchain_community.docstore import InMemoryDocstore
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from langgraph.graph import StateGraph, START
+from langchain_core.embeddings import Embeddings
 
-from scheme.config import PathConfig
+from ._encoder import PretrainedEmbeddings
+from scheme.config import PathConfig, RAGConfig
+
+
+config = RAGConfig()
 
 
 class RAGState(TypedDict):
@@ -22,34 +27,55 @@ class RAGExtractor:
     """
 
     def __init__(self, docs_pool: list[Document], path_config: PathConfig, *, load: bool = False):
-        # TODO: generic embedder from config
-        embeddings = GoogleGenerativeAIEmbeddings( model="models/text-embedding-004" )
+        self._config = path_config
+        # embeddings = GoogleGenerativeAIEmbeddings( model="models/text-embedding-004" )
+        embeddings = PretrainedEmbeddings(config.encoder)
+
+        bm25_retriever = BM25Retriever.from_texts(
+            [d.page_content for d in docs_pool],
+            metadatas=[d.metadata for d in docs_pool],
+        )
+        bm25_retriever.k = 10
+
         if load:
-            self._vector_store = FAISS.load_local(
+            dense_store = FAISS.load_local(
                 path_config.cache_root.as_posix(),
                 embeddings,
                 allow_dangerous_deserialization=True,
             )
         else:
-            index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
-            self._vector_store = FAISS(
-                embedding_function=embeddings,
-                index=index,
-                docstore=InMemoryDocstore(),
-                index_to_docstore_id={},
-            )
+            dense_store = self._build_dense_store(docs_pool, embeddings)
 
-            self._vector_store.add_documents(docs_pool)
-            self._vector_store.save_local(path_config.cache_root.as_posix())
+        self._ret = EnsembleRetriever(
+            retrievers=[
+                dense_store.as_retriever(search_kwargs={"k": 10}),
+                bm25_retriever,
+            ],
+            weights=[0.5, 0.5],
+        )
 
         builder = StateGraph(RAGState).add_node("retrieve", self._retrieve)
         builder.add_edge(START, "retrieve")
         self._graph = builder.compile()
 
 
-    # TODO: search result pre-filtering
+    def _build_dense_store(self, docs_pool: list[Document], embeddings: Embeddings):
+        index = faiss.IndexHNSWFlat(len(embeddings.embed_query("hello world")), 32)
+
+        vectore_store = FAISS(
+            embedding_function=embeddings,
+            index=index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
+        )
+        vectore_store.add_documents(docs_pool)
+        vectore_store.save_local(self._config.cache_root.as_posix())
+
+        return vectore_store
+
+
     async def _retrieve(self, state: RAGState) -> RAGState:
-        relevant_docs = await self._vector_store.asimilarity_search(state["question"], k=10)
+        relevant_docs = await self._ret.ainvoke(state["question"])
         return state | {
             "retrieved": [d.metadata["source"] for d in relevant_docs]
         }
